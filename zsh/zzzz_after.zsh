6e2030c8244bea297dad09c979cd3425340cfcc5
Sherry Seibel <sherry.seibel@drugdev.com>
2018-10-02 14:47:47 -0400

    Add aliases for tmux and allow copy/paste between panes

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -4,3 +4,19 @@ if [ -d $HOME/.zsh.after/ ]; then
     for config_file ($HOME/.zsh.after/*.zsh) source $config_file
   fi
 fi
+
+alias tns='tmux new -s'
+alias tks='tmux kill-session -t'
+alias tls='tmux ls'
+alias tat='tmux attach -t'
+alias tkall='tmux kill-server'
+
+# For copy/pasting across vim and tmux https://robots.thoughtbot.com/how-to-copy-and-paste-with-tmux-on-mac-os-x
+#!/bin/sh
+
+while true; do
+  if test -n "`tmux showb 2> /dev/null`"; then
+    tmux saveb -|pbcopy && tmux deleteb
+  fi
+  sleep 0.5
+done
