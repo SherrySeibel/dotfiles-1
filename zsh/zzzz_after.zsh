70f2f33beccc21a90aa7128f5877c8a4aca035a9
Sherry Seibel <sherry.seibel@drugdev.com>
2019-08-28 14:35:28 -0400

    Create DrugDev specific aliases for Platform db setup

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -16,10 +16,10 @@ alias tkall='tmux kill-server'
 # The loop was commented out because it was always returning true
 
 # while true; do
-  if test -n "`tmux showb 2> /dev/null`"; then
-    tmux saveb -|pbcopy && tmux deleteb
-  fi
-  sleep 0.5
+if test -n "`tmux showb 2> /dev/null`"; then
+  tmux saveb -|pbcopy && tmux deleteb
+fi
+sleep 0.5
 # done
 
 # Additional git shortcuts
@@ -32,3 +32,33 @@ alias dbm='rake db:migrate'
 alias dbrb='rake db:rollback'
 alias dbd='rake db:drop'
 alias dbc='rake db:create'
+
+# Platform specific
+
+# To reset the database copletely then seed:
+db_setup() {
+  rails db:reset &&
+    RAILS_ENV=test rails epernicus:rebuild_views &&
+    rails volumize:all &&
+    rails tn:keycloak_client:sync_keycloak_clients &&
+    rails tn:keycloak_user:sync_keycloak_users PASSWORD=Test12345\! FORCE_SYNC=ENABLE
+  }
+
+# Additional bash
+
+follow() {
+    tail -f log/development.log | grep -i "$@"
+} # Use this to search for something specific. eg: follow ERROR
+
+# DrugDev specific Docker containers
+
+alias dstop='docker stop `docker ps -q`'
+alias dset='ansible-playbook -vvv --tags "clean_docker, start_infrastructure, init_platform, start_platform, start_selenium" setup.yml'
+alias diset='ansible-playbook -vvv --tags "setup-docker" --skip-tags "never" setup.yml'
+
+# Stop all current containers and remove all images. This will ensure a clean slate as you transition to the new setup.
+
+dpurge() {
+    docker stop `docker ps -q` &&
+    docker system prune -a
+}
