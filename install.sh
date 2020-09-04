dc63a28811371906d6e3759154653053a6561320
Sherry Seibel <sherry.seibel@drugdev.com>
2020-09-04 12:25:38 -0400

    Add remote named `old` linked to original skwp repo if still want their latest

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/.yadr" ]; then
-    echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/SherrySeibel/dotfiles-1.git "$HOME/.yadr"
+    echo "Installing Sherry's YADR for the first time"
+    git clone https://github.com/SherrySeibel/dotfiles-1.git "$HOME/.yadr"
+    # Create pull from original repo if still want access
+    git remote add old https://github.com/skwp/dotfiles
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
 else
-    echo "YADR is already installed"
+    echo "Sherry's YADR is already installed"
 fi
